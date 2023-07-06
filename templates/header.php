c45d945d672fa0550032eed8b7d6cc8031b4cc7b
JonathanAristides <Jonathan.nuel98@gmail.com>
2023-07-06 13:44:27 +0200

    implements display of pizzas from db

--- templates/header.php
+++ templates/header.php
@@ -12,6 +12,7 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<style>
 
+
       .brand {
           background: #cbb09c !important;
       }
